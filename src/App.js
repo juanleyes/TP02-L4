06d998e6c891f4645a59b44c6bc987d795823653
Juan <juanleyes0610@gmail.com>
2026-09-12 16:46:48 -0300

    config para poder ejecutar el github Pages con HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router";
 import Inicio from "./pages/Inicio";
 import Servicios from "./pages/Servicios";
 import Contacto from "./pages/Contacto";
@@ -7,14 +7,14 @@ import Error from "./pages/Error";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Inicio />} />
           <Route path="/servicios" element={<Servicios />} />
           <Route path="/contacto" element={<Contacto />} />
           <Route path="*" element={<Error />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
