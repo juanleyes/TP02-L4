13ef9f4d59b5ac037935a1a457333ef19a4b6bcb
Juan <juanleyes0610@gmail.com>
2026-09-12 16:51:50 -0300

    correccion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import Inicio from "./pages/Inicio";
 import Servicios from "./pages/Servicios";
 import Contacto from "./pages/Contacto";
@@ -7,14 +7,14 @@ import Error from "./pages/Error";
 function App() {
   return (
     <>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Inicio />} />
           <Route path="/servicios" element={<Servicios />} />
           <Route path="/contacto" element={<Contacto />} />
           <Route path="*" element={<Error />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </>
   );
 }
